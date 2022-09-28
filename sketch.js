944514f2ef56b47dbc04ab11ceeed89d4b2c27e6
Michal Fitrzyk <78456035+mfitrz@users.noreply.github.com>
2022-09-28 16:58:34 -0500

    First change (test)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
+'#703910', //top 0
+'#ffed00', //middle top 1
 '#A36C33', //middle strip 2
 '#042060', //middle bottom 3
 '#3778AE' //bottom 4
